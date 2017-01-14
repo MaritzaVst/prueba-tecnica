e1a3d8619a44c9f498e829348d1ad090fb7cdade
maritzavst <maritzasintitafur@gmail.com>
2017-01-14 05:51:51 -0500

    Sort de numeros listo

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+	/*Elimina los números repetidos*/
+	function removeDuplicates(arr) {
+    var seen = {};
+    var arrayClean = [];
+    for (var i = 0; i < arr.length; i++) {
+        if (!(arr[i] in seen)) {
+            arrayClean.push(arr[i]);
+            seen[arr[i]] = true;
+        }
+    }
+    return arrayClean;
+	}
+	/*Obtiene el valor del input al hacer enter*/
+	$("input").keypress(function(event){
+			if (event.wich == 13 || event.keyCode == 13) {
+				var number = $("input").val().split("").sort();
+				var arrayClean = removeDuplicates(number);
+			}
+	});
+
+	/*Inserta */
+	
+});
