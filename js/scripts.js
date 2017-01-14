b3962355805fe1b9509b30445b19e84c4a21d6da
maritzavst <maritzasintitafur@gmail.com>
2017-01-14 06:26:19 -0500

    Resultado se muestra con animacion de texto

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
 	/*Elimina los números repetidos*/
 	function removeDuplicates(arr) {
     var seen = {};
@@ -11,16 +12,33 @@ $(document).ready(function(){
     }
     return arrayClean;
 	}
+
+	function result() {
+		/*Muestra seccion de resultado*/
+		$(".result").removeClass("hidden");
+
+		/*Ejecuta el scroll de seccion*/
+    $('html, body').stop().animate({
+        scrollTop: $(".result").offset().top
+    }, 1000);
+
+    setTimeout(function(){
+    	/*Inserta resultado(arrayClean) en nueva seccion*/
+			$(".number-result").text(arrayClean);
+			
+			/*Efecto de animacion con textillate*/
+    	$(function () {
+			  $('.tlt').textillate();
+			})
+    }, 700);
+	}
+
 	/*Obtiene el valor del input al hacer enter*/
 	$("input").keypress(function(event){
 			if (event.wich == 13 || event.keyCode == 13) {
 				var number = $("input").val().split("").sort();
-				var arrayClean = removeDuplicates(number);
-
-				/*Ejecuta el scroll de seccion*/
-		    $('html, body').stop().animate({
-		        scrollTop: $(".result").offset().top
-		    }, 1000);
+				var arrayClean = removeDuplicates(number).join("");
+				result();
 			}
 	});
 });
